c6e0e7652987fe5f5ac2d5c0ae02ac7eaa04c085
shoaib923 <sk5351141@gmail.com>
2025-08-21 19:19:12 +0500

    connect to MongoDb Atlas by connection string

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app=express();
+dotenv.config();
+const app = express();
 
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-app.listen (3000,()=>{
-    console.log("Server is running on port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
